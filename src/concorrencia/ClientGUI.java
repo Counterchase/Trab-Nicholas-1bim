caf33659cf52de8b9b9fd58f27120c132ea011fc
tingo <tingo@DESKTOP-UO9FT4C>
2019-10-08 23:23:16 -0400

    Commit 2 do tingolopes

--- src/concorrencia/ClientGUI.java
+++ src/concorrencia/ClientGUI.java
@@ -5,8 +5,6 @@
  */
 package concorrencia;
 
-import concorrencia.Cliente;
-import concorrencia.ClientGUI;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.Font;
@@ -15,7 +13,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
